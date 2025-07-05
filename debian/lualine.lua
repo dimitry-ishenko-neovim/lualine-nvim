a4e017d916397e8681a698ed6e4f4f1f9c7dd948
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-07-05 16:01:22 -0400

    lualine.lua: customize

--- debian/lualine.lua
+++ debian/lualine.lua
@@ -1,17 +1,12 @@
 require("lualine").setup {
-    options = {
-        theme = "powerline",
-    },
-    sections = {
+    options = {theme = "powerline"},
+    sections= {
         lualine_b = {
             {"branch", icon = "󰊢"},
-            "diff",
-            {"diagnostics", symbols = { error = " ", warn = " ", info = " ", hint = " " }}
+            {"diff", symbols = {modified = "≠"}},
+            {"diagnostics", symbols = {error = " ", warn = " ", info = " ", hint = " "}}
         },
-        lualine_c = { {"filename", symbols = {
-            modified= "*",
-            readonly= "󰍁",
-        } } }
+        lualine_c = { {"filename", symbols = {modified= "*", readonly= "󰍁"}} }
     },
     tabline = {
         lualine_a = { {"buffers", symbols = {modified = "*"}} },
